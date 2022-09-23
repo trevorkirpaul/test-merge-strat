ffe4487cd52884239573b932b09a75d7252178c0
Trevor Kirpaul <trevorkirpaul@gmail.com>
2022-09-23 12:07:04 -0400

    - 1

---
+++ App.js
@@ -0,0 +1,13 @@
+const username = "Trevor";
+const age = 35;
+const hometown = "Hopewell";
+
+const App = () => {
+  return (
+    <div>
+      <h1>{username}</h1>
+      <h1>{age}</h1>
+      <h1>{hometown}</h1>
+    </div>
+  );
+};
