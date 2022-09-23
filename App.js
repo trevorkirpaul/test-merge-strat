50f61f47e8785d1b13d5ccac6c940e4efd730fdc
Trevor Kirpaul <30737677+trevorkirpaul@users.noreply.github.com>
2022-09-23 12:19:28 -0400

    Feature 1 release (#3)
    * - f132123
    
    * 13123
    
    * fsdfsd

--- App.js
+++ App.js
@@ -7,7 +7,14 @@ const App = () => {
     <div>
       <h1>{username}</h1>
       <h1>{age}</h1>
+      <h1>{age}</h1>
+      <h1>{username}</h1>
+      <h1>{age}</h1>
       <h1>{hometown}</h1>
+      <h1>{hometown}</h1>
+      <h1>{hometown}</h1>
+      <h1>{hometown}</h1>
+      {/* this is a test */}
     </div>
   );
 };
